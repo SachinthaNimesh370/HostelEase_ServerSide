78f3868b77989ab0c8f9005ee21972d8117a602c
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2025-05-26 12:23:29 +0530

    Initialize springboot template

---
+++ src/main/java/com/uoj/HostelEase/HostelEaseApplication.java
@@ -0,0 +1,13 @@
+package com.uoj.HostelEase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostelEaseApplication.class, args);
+	}
+
+}
