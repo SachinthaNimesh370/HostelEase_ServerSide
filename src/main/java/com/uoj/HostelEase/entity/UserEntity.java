28c161e7efc09a18a98c54266cf3cc332e1dcf85
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2025-06-01 11:58:55 +0530

    modifierd user entity class

--- src/main/java/com/uoj/HostelEase/entity/UserEntity.java
+++ src/main/java/com/uoj/HostelEase/entity/UserEntity.java
@@ -1,9 +1,13 @@
-package com.uoj.HostelEase.dto;
+package com.uoj.HostelEase.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 
+@Entity
 @Data
-public class UserRegRequestDTO {
+public class UserEntity {
+    @Id
     int id;
     String reg_no;
     String f_name;
