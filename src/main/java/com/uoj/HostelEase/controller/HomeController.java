fbc274b2d95c12d0ec494c130cb2d8d4fc19b8d7
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2025-06-01 11:27:59 +0530

    implemented Configering class

--- src/main/java/com/uoj/HostelEase/controller/HomeController.java
+++ src/main/java/com/uoj/HostelEase/controller/HomeController.java
@@ -1,11 +1,9 @@
 package com.uoj.HostelEase.controller;
 
 import com.uoj.HostelEase.service.JWTService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/home")
 public class HomeController {
