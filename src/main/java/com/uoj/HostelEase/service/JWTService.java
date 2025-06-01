ee54399f944b483419aca5330ba73fe9802e6c10
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2025-06-01 12:26:30 +0530

    implement service layer

--- src/main/java/com/uoj/HostelEase/service/JWTService.java
+++ src/main/java/com/uoj/HostelEase/service/JWTService.java
@@ -1,7 +1,9 @@
 package com.uoj.HostelEase.service;
 
+import java.util.Map;
+
 public interface JWTService {
 
-    public String jwtToken();
+    public String jwtToken(String subject, Map<String,String> clams);
     public String getUserName(String token);
 }
