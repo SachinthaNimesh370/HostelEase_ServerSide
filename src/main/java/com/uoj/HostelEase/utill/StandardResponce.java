721962dfbc04d1f303eb0d0a2b89799581138d3b
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2025-06-02 08:57:35 +0530

    change resonse for return role and token

--- src/main/java/com/uoj/HostelEase/utill/StandardResponce.java
+++ src/main/java/com/uoj/HostelEase/utill/StandardResponce.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Map;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -11,4 +13,5 @@ public class StandardResponce {
     private  int code;
     private String message;
     private Object data;
+    private Map<String,String> role;
 }
