4b4ee3a87ee4b0f43b51210e4ddbb828f0e578ef
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2025-06-21 09:02:00 +0530

    New Complain Save Process Is sucess

--- src/main/java/com/uoj/HostelEase/controller/ComplainController.java
+++ src/main/java/com/uoj/HostelEase/controller/ComplainController.java
@@ -1,18 +1,43 @@
 package com.uoj.HostelEase.controller;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.uoj.HostelEase.dto.ComplainDTO;
+import com.uoj.HostelEase.dto.UserLoginResponceDTO;
+import com.uoj.HostelEase.service.ComplainService;
+import com.uoj.HostelEase.utill.ServiceResponse;
+import com.uoj.HostelEase.utill.StandardResponce;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/complain")
 public class ComplainController {
+    private final ComplainService complainService;
+
+    public ComplainController(ComplainService complainService) {
+        this.complainService = complainService;
+    }
 
     @PostMapping("/newcomplain")
-    public String newComplain() {
-        return null;
+    public ResponseEntity<StandardResponce> newComplain(@RequestBody ComplainDTO complainDTO) {
+        ServiceResponse massage = complainService.newComplain(complainDTO);
+        if(massage.isSuccess()) {
+            return new ResponseEntity<StandardResponce>(
+                    new StandardResponce(
+                            200, "Ok", new UserLoginResponceDTO(
+                            massage.getObject(), LocalDateTime.now()), massage.getRole()),
+                    HttpStatus.OK);
+        }else{
+            return new ResponseEntity<StandardResponce>(
+                    new StandardResponce(
+                            400,"Bad", new UserLoginResponceDTO(
+                            massage.getObject(),null),null),
+                    HttpStatus.BAD_REQUEST);
+        }
     }
 }
--- src/main/java/com/uoj/HostelEase/service/IMPL/ComplainServiceIMPL.java
+++ src/main/java/com/uoj/HostelEase/service/IMPL/ComplainServiceIMPL.java
@@ -1,8 +1,43 @@
 package com.uoj.HostelEase.service.IMPL;
 
+import com.uoj.HostelEase.dto.ComplainDTO;
+import com.uoj.HostelEase.entity.ComplainEntity;
+import com.uoj.HostelEase.repo.ComplainRepository;
 import com.uoj.HostelEase.service.ComplainService;
+import com.uoj.HostelEase.utill.ServiceResponse;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ComplainServiceIMPL implements ComplainService {
+    private final ComplainRepository complainRepository;
+    private final ModelMapper modelMapper;
+
+    public ComplainServiceIMPL(ComplainRepository complainRepository, ModelMapper modelMapper) {
+        this.complainRepository = complainRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public ServiceResponse newComplain(ComplainDTO complainDTO) {
+        try {
+            ComplainEntity complainEntity = modelMapper.map(complainDTO, ComplainEntity.class);
+            complainRepository.save(complainEntity);
+            return new ServiceResponse(true,"Complain Saved",null);
+        } catch (Exception e) {
+            System.out.println(e);
+            return new ServiceResponse(false,"Complain Can't Save",null);
+        }
+
+    }
+
+    @Override
+    public ServiceResponse updateComplain(ComplainDTO complainDTO) {
+        return null;
+    }
+
+    @Override
+    public ServiceResponse deleteComplain(ComplainDTO complainDTO) {
+        return null;
+    }
 }
